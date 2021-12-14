b5b077b749c86540d0a4e2fc2b9c4acd609cc7ad
Matt Deal <contact@mattdeal.com.au>
2021-12-14 23:28:30 +1100

    [toDo App]: Adds validation to add button

--- src/componenets/addButton.js
+++ src/componenets/addButton.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import Alert from "react-bootstrap/Alert";
 import uuid from "react-uuid";
 
 function AddItem({ setData }) {
   const [add, addItem] = useState("");
+  const [error, setError] = useState(false);
   console.log(add);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setData((oldArray) => [...oldArray, { name: add, id: uuid() }]);
+    if (add === "") {
+      setError(true);
+    } else {
+      setData((oldArray) => [...oldArray, { name: add, id: uuid() }]);
+    }
   }
 
   return (
@@ -18,12 +24,19 @@ function AddItem({ setData }) {
         placeholder="Type a new item here"
         onChange={(e) => addItem(e.target.value)}
       />
+
       <button
+        type="submit"
         onClick={handleSubmit}
         className=" shadow-xl border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white font-bold p-2 mt-5"
       >
         Add Item
       </button>
+      {error ? (
+        <Alert className="mt-2" variant="danger">
+          You need to enter a Todo
+        </Alert>
+      ) : null}
     </form>
   );
 }
